3a67b887339fb60cced09ff6eb6a54be197ca9aa
Geono <syaokun2744@gmail.com>
2018-04-22 17:11:14 +0900

    Normally working

--- android/app/src/main/java/com/ebaykornstarter/MainApplication.java
+++ android/app/src/main/java/com/ebaykornstarter/MainApplication.java
@@ -3,6 +3,11 @@ package com.ebaykornstarter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.rnfs.RNFSPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +29,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new RNFSPackage(),
+            new RNFirebasePackage(),
+            new RNDeviceInfo(),
+            new ReactNativeConfigPackage(),
             new RNGoogleSigninPackage()
       );
     }
